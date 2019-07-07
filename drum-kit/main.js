1e6f867228ce8b87c762b78bac4a7cd330b91169
Paul Palmieri <palmirouze@gmail.com>
2019-07-07 15:59:54 -0400

    Added all sounds

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -1,32 +1,32 @@
 // store path for sounds
 // all the same for now
 const audio_paths = {
-  65: { key: "A", src: "808-Clap01.wav" },
-  66: { key: "B", src: "808-Clap01.wav" },
-  67: { key: "C", src: "808-Clap01.wav" },
-  68: { key: "D", src: "808-Clap01.wav" },
-  69: { key: "E", src: "808-Clap01.wav" },
-  70: { key: "F", src: "808-Clap01.wav" },
-  71: { key: "G", src: "808-Clap01.wav" },
-  72: { key: "H", src: "808-Clap01.wav" },
-  73: { key: "I", src: "808-Clap01.wav" },
-  74: { key: "J", src: "808-Clap01.wav" },
-  75: { key: "K", src: "808-Clap01.wav" },
-  76: { key: "L", src: "808-Clap01.wav" },
-  77: { key: "M", src: "808-Clap01.wav" },
-  78: { key: "N", src: "808-Clap01.wav" },
-  79: { key: "O", src: "808-Clap01.wav" },
-  80: { key: "P", src: "808-Clap01.wav" },
-  81: { key: "Q", src: "808-Clap01.wav" },
-  82: { key: "R", src: "808-Clap01.wav" },
-  83: { key: "S", src: "808-Clap01.wav" },
-  84: { key: "T", src: "808-Clap01.wav" },
-  85: { key: "U", src: "808-Clap01.wav" },
-  86: { key: "V", src: "808-Clap01.wav" },
-  87: { key: "W", src: "808-Clap01.wav" },
-  88: { key: "X", src: "808-Clap01.wav" },
-  89: { key: "Y", src: "808-Clap01.wav" },
-  90: { key: "Z", src: "808-Clap01.wav" }
+  65: { key: "A", src: "Stick 1.wav" },
+  66: { key: "B", src: "Tom 1.wav" },
+  67: { key: "C", src: "Tom 2.wav" },
+  68: { key: "D", src: "Warm 808.wav" },
+  69: { key: "E", src: "Bass Hit.wav" },
+  70: { key: "F", src: "Clap 1.wav" },
+  71: { key: "G", src: "Clap 2.wav" },
+  72: { key: "H", src: "Clave 1.wav" },
+  73: { key: "I", src: "Conga 1.wav" },
+  74: { key: "J", src: "Conga 2.wav" },
+  75: { key: "K", src: "Conga 3.wav" },
+  76: { key: "L", src: "Cowbell.wav" },
+  77: { key: "M", src: "Hi hats 1.wav" },
+  78: { key: "N", src: "Hi hats 2.wav" },
+  79: { key: "O", src: "Hi hats 3.wav" },
+  80: { key: "P", src: "Kick 1.wav" },
+  81: { key: "Q", src: "Maracas 1.wav" },
+  82: { key: "R", src: "Ramen.wav" },
+  83: { key: "S", src: "Rim 1.wav" },
+  84: { key: "T", src: "Rim 2.wav" },
+  85: { key: "U", src: "Rim 3.wav" },
+  86: { key: "V", src: "Sci Fi Hit.wav" },
+  87: { key: "W", src: "Snare 1.wav" },
+  88: { key: "X", src: "Snare 2.wav" },
+  89: { key: "Y", src: "Snare 3.wav" },
+  90: { key: "Z", src: "Snare 4.wav" }
 };
 
 function generateKeys() {
@@ -77,12 +77,14 @@ function playSoundKeyboard(e) {
 
 function playSoundClick(e) {
   let key, audio;
-  if(e.target.tagName === 'SPAN' || e.target.tagName === 'KBD') {
+  if (e.target.tagName === "SPAN" || e.target.tagName === "KBD") {
     key = e.target.parentNode;
   } else {
     key = e.target;
   }
-  audio = document.querySelector(`audio[data-key="${key.getAttribute('data-key')}"]`);
+  audio = document.querySelector(
+    `audio[data-key="${key.getAttribute("data-key")}"]`
+  );
   playSound(key, audio);
 }
 
